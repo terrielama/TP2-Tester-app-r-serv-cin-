8f560bcf3744060d47558644ea911e9628c99319
Arnaud Fouchet <foucheta@gmail.com>
2026-02-02 07:57:46 +0100

    Fix

--- core/models.py
+++ core/models.py
@@ -3,26 +3,5 @@ from django.db import models
 
 
 class UserType(models.Model):
-<<<<<<< Updated upstream
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_type')
     is_company = models.BooleanField(default=False)
-
-    def json(self):
-        return {
-            "id": self.user.id,
-            "username": self.user.username,
-            "email": self.user.email,
-            "is_company": self.is_company,
-        }
-
-
-class Theater(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='theaters')
-    name = models.CharField(max_length=200)
-    address = models.CharField(max_length=1_000)
-=======
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='user_type'
-    )
-    is_company = models.BooleanField()
->>>>>>> Stashed changes
